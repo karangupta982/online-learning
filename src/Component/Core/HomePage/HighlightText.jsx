5184e2edf0fdff16af30466d8284beb62d06f0ef
Karan gupta <gupta.karan1.gh@gmail.com>
2025-10-01 10:07:03 +0530

    Highlight text fixed

--- src/Component/Core/HomePage/HighlightText.jsx
+++ src/Component/Core/HomePage/HighlightText.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HighlightText = ({text}) => {
   return (
-    <span className="bg-gradient-to-b from-secondary-light via-secondary to-accent-light text-transparent bg-clip-text font-bold">
+    <span className="bg-gradient-to-b from-blue-700 via-caribbeangreen-200 to-richblue-100 text-transparent bg-clip-text font-bold">
       {" "}
       {text}
     </span>
